cefc1975229bb2b31775721924ce12949c7ef496
Wax-Zhong <979413981@qq.com>
2024-08-23 21:39:04 +0800

    ~

--- download.py
+++ download.py
@@ -1,4 +1,4 @@
 #模型下载
 from modelscope import snapshot_download
-model_dir = snapshot_download('mirror013/mxbai-embed-large-v1')
+model_dir = snapshot_download('mirror013/mxbai-embed-large-v1', cache_dir='./model')
 print(model_dir)
